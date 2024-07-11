ec7fb7f5bdc763541d027e74b751ccf0aaa1bcbc
menachemlemmer <nmmlemmer@gmail.com>
2024-07-11 16:06:23 -0400

    Add basic styling to garden

--- frontend/src/components/Garden.jsx
+++ frontend/src/components/Garden.jsx
@@ -1,12 +1,13 @@
 function Garden(props) {
   console.log(props);
   return (
-    <main>
+    <main className="mx-4">
       <h1>Garden</h1>
       <ul>
         {props.plants.map((plant) => (
-          <li key={plant._id}>
-            <h2 className="text-2xl">{plant.commonName}</h2>
+          <li className="m-2 p-4" key={plant._id}>
+            <h2 className="my-2 text-2xl">{plant.commonName}</h2>
+            <img className="w-1/4" src={plant.image} alt={plant.commonName} />
             <p>{plant.description}</p>
           </li>
         ))}
