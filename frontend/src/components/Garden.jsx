fc9d7afaa82f4556b8b5bbcf4a887ff322ec5960
menachemlemmer <nmmlemmer@gmail.com>
2024-07-15 10:50:37 -0400

    Add card styling

--- frontend/src/components/Garden.jsx
+++ frontend/src/components/Garden.jsx
@@ -3,11 +3,19 @@ function Garden(props) {
   return (
     <main className="mx-4">
       <h1>Garden</h1>
-      <ul>
+      <ul className="flex flex-wrap">
         {props.plants.map((plant) => (
-          <li className="m-2 p-4" key={plant._id}>
-            <h2 className="my-2 text-2xl">{plant.commonName}</h2>
-            <img className="w-1/4" src={plant.image} alt={plant.commonName} />
+          <li
+            className="m-2 flex h-96 w-80 flex-col overflow-clip rounded-md border-2 border-black p-4"
+            key={plant._id}
+          >
+            <h2 className="my-2 text-2xl">{plant.userPlantName}</h2>
+            <h3 className="relative top-64 my-2 text-xl text-white">
+              {plant.commonName}
+            </h3>
+            <img className="size-64" src={plant.image} alt={plant.commonName} />
+            <p>Watering Frequency: {plant.wateringFrequency}</p>
+            <p>Sunlight: {plant.sunlight}</p>
             <p>{plant.description}</p>
           </li>
         ))}
